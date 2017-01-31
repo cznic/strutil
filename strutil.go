43a89592ed56c227c7fdb1fcaf7d1d08be02ec54
Jan Mercl <0xjnml@gmail.com>
2017-01-31 13:30:52 +0100

    Gopath: Add support for plan9.

--- strutil.go
+++ strutil.go
@@ -656,6 +656,8 @@ func Gopath() string {
 
 	// go1.8: https://github.com/golang/go/blob/74628a8b9f102bddd5078ee426efe0fd57033115/doc/code.html#L122
 	switch runtime.GOOS {
+	case "plan9":
+		return os.Getenv("home")
 	case "windows":
 		return filepath.Join(os.Getenv("USERPROFILE"), "go")
 	default:
